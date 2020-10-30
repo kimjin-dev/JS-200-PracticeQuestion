5120b3c98ec508737cbb8e78c312626a70f7c4b3
JinKim <kimjin.dev@gmail.com>
2020-10-30 15:58:46 +0900

    JS 200 Practice Question Book 6-10

---
+++ part1/js200_006.js
@@ -0,0 +1,9 @@
+//표현식; 값을 생성할 때, 변수 또는 함수 인자로 값을 넣을 때 표현식을 선언
+console.log((3+12)/5);
+//명령문; 일종의 행위를 선언하거나 행위를 수행하게 할 때 (ej; if,if-else,for,switch)
+var boo = true;
+if(boo){
+    console.log('TRUE!!');
+}else{
+    console.log('FALSE!!');
+}
